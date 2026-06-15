4ac3013d2385e22122038d1fdc8ed5205ce6b7a4
Vipul Kannaujiya <vipulkannaujiya958@gmail.com>
2026-06-15 20:41:23 +0530

    Chapter 2 Practice Set

---
+++ Chapter 2 Practice Set.js
@@ -0,0 +1,5 @@
+// Q1 - Use logical operators to find whether the age of a person lies between 10 to 20.
+
+const prompt = require('prompt-sync')(); // terminal input ke liye
+let a = prompt("Enter Your Age : ");
+console.log("You Entered = ", a);
