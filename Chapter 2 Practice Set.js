d7dd70f4dd4b50fe0010c8543bb3d74d5bda2303
Vipul Kannaujiya <vipulkannaujiya958@gmail.com>
2026-06-17 14:46:25 +0530

    Chapter 2 Practice Set

--- Chapter 2 Practice Set.js
+++ Chapter 2 Practice Set.js
@@ -3,3 +3,15 @@
 const prompt = require('prompt-sync')(); // terminal input ke liye
 let a = prompt("Enter Your Age : ");
 console.log("You Entered = ", a);
+
+if (a > 0 && a <= 100){
+    if (a >= 10 && a <= 20){
+        console.log("You Are Lies Between 10 To 20");
+    }
+    else{
+        console.log("You Are Not Lies Between 10 To 20");
+    }
+}
+else{
+    console.log("Invalid Age!!!");
+}
