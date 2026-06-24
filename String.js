3142b5ced83ffa208f03336ea2acf45c236dfce3
Vipul Kannaujiya <vipulkannaujiya958@gmail.com>
2026-06-24 16:16:02 +0530

    String Practice

---
+++ String.js
@@ -0,0 +1,32 @@
+// String are used to store and manipulate text string can be created using the following Syntax.
+let name = "Vipul"
+console.log(name)
+console.log(name.length)
+
+let a = `Vipul`
+console.log(a)
+
+let b = `This is ${name}`
+console.log(b)
+
+// CHANGING THE CASE OF CHARACTER
+
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(1,3))
+console.log(name.replace("pul", "pin"))
+
+let c = "Kannaujiya"
+console.log(name.concat( "'s cast is ", c))
+
+let d = "         Vishal           "
+console.log(d)
+console.log(d.trim())
+
+let wf = "Versha"
+console.log(wf[0])
+console.log(wf[1])
+console.log(wf[2])
+console.log(wf[3])
+console.log(wf[4])
+console.log(wf[5])
